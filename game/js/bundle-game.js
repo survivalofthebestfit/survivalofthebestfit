2dd187224731bc4f33be876f54cf77f4a1618628
Alia ElKattan <aae322@nyu.edu>
2019-06-12 03:16:14 +0200

    gh pages release

--- game/js/bundle-game.js
+++ game/js/bundle-game.js
@@ -104944,12 +104944,11 @@ var gameFSM = new machina.Fsm({
   states: {
     uninitialized: {
       startGame: function startGame() {
-        // this.transition('titleStage');
-        // this.transition('smallOfficeStage');
+        this.transition('titleStage'); // this.transition('smallOfficeStage');
         // this.transition('mlTransitionStage');
         // this.transition('mlTrainingStage');
         //this.transition('mlLabStage');
-        this.transition('gameBreakdown');
+        //this.transition('gameBreakdown');
       }
     },
 
